ce43a1be5d455ce7d8ce75e33655a14e582847e7
Bui Duc Huy <duchuy.124dk@gmail.com>
2023-02-16 16:01:48 +0700

    update code

--- index.js
+++ index.js
@@ -4,26 +4,19 @@ const bs58 = require("bs58")
 const { Buffer } = require("buffer")
 
 const enc = new TextEncoder()
-const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'), 'confirmed')
-
-const SEED = "cream lazy drastic tongue dances"
-const seedEncoded =  enc.encode(SEED)
-const keyPair = solanaWeb3.Keypair.fromSeed(seedEncoded)
-
-const anotherKeyPair = solanaWeb3.Keypair.generate()
+const connection = new solanaWeb3.Connection("http://127.0.0.1:8899", 'confirmed')
 
 // request airdrop
-async function getAirdrop() {
+async function getAirdrop(publicKey, amount) {
     const airdropSign = await connection.requestAirdrop(
-        keyPair.publicKey,
-        solanaWeb3.LAMPORTS_PER_SOL
+        publicKey,
+        amount
     )
 
     await connection.confirmTransaction(airdropSign);
 
-    console.log(airdropSign)
+    console.log("Request Airdrop", amount, "SOL for wallet", publicKey.toString(), "\n")
 }
-//getAirdrop()
 
 // return buffer type: encoded data as base58 type
 function encode(type, fields) {
@@ -35,61 +28,55 @@ function encode(type, fields) {
     return data
 }
 
-// return object type: data decode
-function decode(type, buffer) {
-    let data
-    try {
-        data = type.layout.decode(buffer)
-    } catch (err) {
-        throw new Error("invalid instruction: " + err)
-    }
-    
-    if (data.instruction != type.index) {
-        throw new Error(`invalid instruction; instruction index mismatch ${data.instruction} != ${type.index}`)
-    }
-
-    return data
-}
-
-// create layout
-const layout = BufferLayout.struct([
-    BufferLayout.u32('instruction'),
-    BufferLayout.ns64('lamports'),
-])
-
-// index = 2 is transfer native token in system program
-const type = {
-    index: 2,
-    layout 
-}
-
-const fields = {
-    lamports: 1
-}
-
-const dataEncoded = encode(type, fields)
-//const dataDecoded = decode(type, dataEncoded)
-
-// keys
-const keys = [
-    {pubkey: keyPair.publicKey, isSigner: true, isWritable: true},
-    {pubkey: anotherKeyPair.publicKey, isSigner: false, isWritable: true},
-]
-
-// create instruction
-const transferInstruction =  new solanaWeb3.TransactionInstruction({
-    keys,
-    programId: solanaWeb3.SystemProgram.programId,
-    data: dataEncoded
-})
-
-// create transaction
-const transaction = new solanaWeb3.Transaction().add(transferInstruction)
-
 // send transaction
 async function sendTransaction() {
-    const signature = await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [keyPair])
-    console.log("signature:", signature)
+    const SEED = "cream lazy drastic tongue dances"
+
+    const TerryWallet = solanaWeb3.Keypair.fromSeed(enc.encode(SEED))
+    console.log("Terry Address", TerryWallet.publicKey.toString(), "\n")
+    await getAirdrop(TerryWallet.publicKey, 2 * solanaWeb3.LAMPORTS_PER_SOL)
+
+    const DaisyWallet = solanaWeb3.Keypair.generate()
+    console.log("Daisy Address", DaisyWallet.publicKey.toString(), "\n")
+
+
+    // create layout
+    const layout = BufferLayout.struct([
+        BufferLayout.u32('instruction'),
+        BufferLayout.ns64('lamports'),
+    ])
+
+    // index = 2 is transfer native token in system program
+    const type = {
+        index: 2,
+        layout 
+    }
+
+    const fields = {
+        lamports: solanaWeb3.LAMPORTS_PER_SOL 
+    }
+
+    const dataEncoded = encode(type, fields)
+    //const dataDecoded = decode(type, dataEncoded)
+
+    // keys
+    const keys = [
+        {pubkey: TerryWallet.publicKey, isSigner: true, isWritable: true},
+        {pubkey: DaisyWallet.publicKey, isSigner: false, isWritable: true},
+    ]
+
+    // create instruction
+    const transferInstruction =  new solanaWeb3.TransactionInstruction({
+        keys,
+        programId: solanaWeb3.SystemProgram.programId,
+        data: dataEncoded
+    })
+
+    // create transaction
+    const transaction = new solanaWeb3.Transaction().add(transferInstruction)
+    const signature = await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [TerryWallet])
+
+    console.log("Transfer", solanaWeb3.LAMPORTS_PER_SOL,"SOL From Terry To Daisy", signature, "\n")
 }
 
 sendTransaction()
